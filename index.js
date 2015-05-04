4f9faddf52d8fcd4fd09291f00e8689d1aa7017a
Jeff Jewiss <jeffjewiss@gmail.com>
2015-05-03 22:46:13 -0400

    Follows new postcss runner guidelines
    * uses async API
    * special output for CSS syntax errors
    * shows plugin warnings
    
    Closes #1.

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var fs = require('fs');
 var mkdirp = require('mkdirp');
 var CachingWriter = require('broccoli-caching-writer');
 var postcss = require('postcss');
+var CssSyntaxError = require('postcss/lib/css-syntax-error');
 
 function PostcssCompiler (inputTrees, inputFile, outputFile, plugins) {
-    if (!(this instanceof PostcssCompiler)) {
+    if ( !(this instanceof PostcssCompiler) ) {
         return new PostcssCompiler(inputTrees, inputFile, outputFile, plugins);
     }
 
-    if (!Array.isArray(inputTrees)) {
+    if ( !Array.isArray(inputTrees) ) {
         throw new Error('Expected array for first argument - did you mean [tree] instead of tree?');
     }
 
@@ -27,27 +28,42 @@ PostcssCompiler.prototype.updateCache = function (includePaths, destDir) {
     var toFilePath = path.join(destDir, this.outputFile);
     var fromFilePath = path.join(includePaths[0], this.inputFile);
 
-    if (!this.plugins || this.plugins.length < 1) {
+    if ( !this.plugins || this.plugins.length < 1 ) {
         throw new Error('You must provide at least 1 plugin in the plugin array');
     }
 
     var processor = postcss();
     var css = fs.readFileSync(fromFilePath, 'utf8');
-
-    this.plugins.forEach(function (plugin) {
-        var options = plugin.options || {};
-        processor.use(plugin.module(options));
-    });
-
-    var result = processor.process(css, {
+    var options = {
         from: fromFilePath,
         to: toFilePath,
         map: { inline: false }
+    };
+
+    this.plugins.forEach(function (plugin) {
+        var pluginOptions = plugin.options || {};
+        processor.use(plugin.module(pluginOptions));
     });
 
-    mkdirp.sync(path.dirname(toFilePath));
+    var processPromise = processor.process(css, options)
+        .then(function (result) {
+            mkdirp.sync(path.dirname(toFilePath));
+            fs.writeFileSync(toFilePath, result.css);
+        })
+        .then(function (result) {
+            result.warnings().forEach(function (warn) {
+                process.stderr.write(warn.toString());
+            });
+        })
+        .catch(function (error) {
+            if ( error instanceof CssSyntaxError ) {
+                process.stderr.write(error.message + error.showSourceCode());
+            } else {
+                throw error;
+            }
+        });
 
-    fs.writeFileSync(toFilePath, result.css);
+
 };
 
 module.exports = PostcssCompiler;
